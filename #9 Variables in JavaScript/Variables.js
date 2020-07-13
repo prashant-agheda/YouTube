1d223ddbd55118fd9802c5277e7630c087ab9662
Prashant Agheda <56477206+prashant-agheda@users.noreply.github.com>
2020-07-13 21:33:45 +0530

    JS File

---
+++ #9 Variables in JavaScript/Variables.js
@@ -0,0 +1,7 @@
+// Separate JavaScript File that is executed using node-JS in VS-Code using the Command : "node filename.js" in Terminal.
+
+var pen_color = "Blue";
+console.log("\nPen color is :", pen_color);
+
+let pen2 = "Red";
+console.log("\nPen 2 Color is :" + pen2);
